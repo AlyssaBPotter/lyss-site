61c8539f9efd22e7d78ecc3a6b140b5659e266b3
Nathan Potter <natepotter95@gmail.com>
2023-08-26 20:33:49 -0700

    newer

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,27 +3,27 @@ import { Carousel } from 'react-carousel-minimal';
 const CarouselGallery = () => {
  const data = [
     {
-      image: "/books/best-thing/Best-Thing.png",
+      image: "./books/best-thing/Best-Thing.png",
       caption: "The Best Thing"
     },
     {
-      image: "/books/every-little-thing/Every-Little-Thing.png",
+      image: "./books/every-little-thing/Every-Little-Thing.png",
       caption: "Every Little Thing"
     },
     {
-      image: "/books/ghost-of-you/Ghost-of-You.png",
+      image: "./books/ghost-of-you/Ghost-of-You.png",
       caption: "Ghost of You"
     },
     {
-      image: "/books/lose-you/Lose-You.png",
+      image: "./books/lose-you/Lose-You.png",
       caption: "Lose You"
     },
     {
-      image: "/books/second-chances/Second-Chances.png",
+      image: "./books/second-chances/Second-Chances.png",
       caption: "Second Chances"
     },
     {
-      image: "/books/truth-or-dare/Truth-or-Dare.png",
+      image: "./books/truth-or-dare/Truth-or-Dare.png",
       caption: "Truth or Dare"
     },
 
