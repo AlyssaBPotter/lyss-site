b2278b90d14d0dff6e74f65cddb71732a134b7a6
Nathan Potter <natepotter95@gmail.com>
2023-08-26 19:40:13 -0700

    update

--- src/App.js
+++ src/App.js
@@ -6,18 +6,9 @@ import BookPage from "./pages/BookPage";
 import UpcomingPage from "./pages/UpcomingPage";
 import AboutPage from "./pages/AboutPage";
 import Footer from "./components/Footer";
-// import { useMediaQuery } from "react-responsive";
-
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  // const isDesktopOrLaptop = useMediaQuery({
-  //   query: "(min-width: 1224px)",
-  // });
-  // const isBigScreen = useMediaQuery({ query: "(min-width: 1824px)" });
-  // const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
-  // const isPortrait = useMediaQuery({ query: "(orientation: portrait)" });
-  // const isRetina = useMediaQuery({ query: "(min-resolution: 2dppx)" });
 
   return (
     <Router basename="/lyss-site">
@@ -27,9 +18,9 @@ function App() {
           <Routes>
             <Route path="/" exact element={<HomePage />} />{" "}
             {/* 'exact' to only render this page when the path is exactly that */}
-            <Route path="/books" element={<BookListPage />} />
-            <Route path="/upcoming" element={<UpcomingPage />} />
-            <Route path="/about" element={<AboutPage />} />
+            <Route path="/books/" element={<BookListPage />} />
+            <Route path="/upcoming/" element={<UpcomingPage />} />
+            <Route path="/about/" element={<AboutPage />} />
             <Route path="/book/:id" element={<BookPage />} />
           </Routes>
           <Footer />
