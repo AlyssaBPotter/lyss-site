660f9dcc9aa462474a6b90218e2f9df6ee96383c
Nathan Potter <natepotter95@gmail.com>
2023-08-26 19:19:49 -0700

    update

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   // const isRetina = useMediaQuery({ query: "(min-resolution: 2dppx)" });
 
   return (
-    <Router>
+    <Router basename="/lyss-site">
       <div className="container">
         <div className="app">
           <Header />
@@ -34,15 +34,6 @@ function App() {
           </Routes>
           <Footer />
         </div>
-
-        {/* <div>
-          <h1>Device Test!</h1>
-          {isDesktopOrLaptop && <p>You are a desktop or laptop</p>}
-          {isBigScreen && <p>You have a huge screen</p>}
-          {isTabletOrMobile && <p>You are a tablet or mobile phone</p>}
-          <p>Your are in {isPortrait ? "portrait" : "landscape"} orientation</p>
-          {isRetina && <p>You are retina</p>}
-        </div> */}
       </div>
     </Router>
   );
